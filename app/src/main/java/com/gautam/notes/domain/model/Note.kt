5afe7352b4b6277d734574ed151068867496d6df
Gautam <gautamhazarika01@gmail.com>
2023-09-03 14:54:29 +0530

    Added dependencies

--- app/src/main/java/com/gautam/notes/domain/model/Note.kt
+++ app/src/main/java/com/gautam/notes/domain/model/Note.kt
@@ -1,6 +1,12 @@
 package com.gautam.notes.domain.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class Note(
     val title: String,
-    val content: String
+    val content: String,
+    @PrimaryKey val id: Int? = null
+
 )

8997d475ee4c9a0618cce219d287d0b690e9c397
Gautam <gautamhazarika01@gmail.com>
2023-09-03 15:03:14 +0530

    Added dependencies

--- app/src/main/java/com/gautam/notes/data/repository/NoteRepository.kt
+++ app/src/main/java/com/gautam/notes/data/repository/NoteRepository.kt
@@ -1,4 +1,23 @@
 package com.gautam.notes.data.repository
 
-class NoteRepository {
+import com.gautam.notes.data.data_source.NoteDao
+import com.gautam.notes.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
+class NoteRepository(private val dao: NoteDao) {
+     fun getNotes(): Flow<List<Note>> {
+        return dao.getNotes()
+    }
+
+     suspend fun getNoteById(id: Int): Note? {
+        return dao.getNoteById(id)
+    }
+
+     suspend fun insertNote(note: Note) {
+        dao.insertNote(note)
+    }
+
+     suspend fun deleteNote(note: Note) {
+        dao.deleteNote(note)
+    }
 }

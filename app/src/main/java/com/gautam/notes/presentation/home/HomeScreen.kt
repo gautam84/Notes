232cfd391df69cdaf610b83c374c85e38fa39047
Gautam <gautamhazarika01@gmail.com>
2023-09-03 15:22:22 +0530

    Added features

--- app/src/main/java/com/gautam/notes/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/gautam/notes/presentation/home/HomeScreen.kt
@@ -2,22 +2,58 @@ package com.gautam.notes.presentation.home
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.gautam.notes.domain.model.Note
 
 @Composable
 fun HomeScreen() {
-    Column (
-        modifier = Modifier.padding(8.dp),
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp),
         verticalArrangement = Arrangement.Center
-            ){
+    ) {
+        val list = listOf(Note("Note1", "Description1"), Note("Note2", "Description2"),Note("Note3", "Description3"),)
 
-        LazyColumn(){
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            contentPadding = PaddingValues(8.dp)
+        ) {
+            items(list.size){
+                NoteCard(note = list[it])
+            }
+
 
         }
+
+    }
+}
+
+@Composable
+fun NoteCard(
+    note: Note
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
+    ) {
+        Text(text = note.title, style = MaterialTheme.typography.titleMedium)
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = note.content)
+        
 
     }
 }

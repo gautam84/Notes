8078229b062a8e3a92bd19711aff4f609847d3be
Gautam <gautamhazarika01@gmail.com>
2023-09-03 15:35:23 +0530

    Added features

--- app/src/main/java/com/gautam/notes/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/gautam/notes/presentation/home/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.gautam.notes.presentation.home
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
@@ -9,38 +10,67 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material3.Card
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.gautam.notes.domain.model.Note
 
 @Composable
 fun HomeScreen() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(8.dp),
-        verticalArrangement = Arrangement.Center
-    ) {
-        val list = listOf(Note("Note1", "Description1"), Note("Note2", "Description2"),Note("Note3", "Description3"),)
 
-        LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            contentPadding = PaddingValues(8.dp)
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomEnd
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp),
+            verticalArrangement = Arrangement.Center
         ) {
-            items(list.size){
-                NoteCard(note = list[it])
+            val list = listOf(
+                Note("Note1", "Description1"),
+                Note("Note2", "Description2"),
+                Note("Note3", "Description3"),
+            )
+
+
+
+
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                contentPadding = PaddingValues(8.dp)
+            ) {
+                item {
+                    Text("Your Notes...")
+                    Spacer(modifier = Modifier.height(8.dp))
+                }
+                items(list.size) {
+                    NoteCard(note = list[it])
+                }
+
+
             }
 
-
         }
-
+        Column(modifier = Modifier.padding(8.dp)) {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Outlined.Add, contentDescription = null)
+            }
+        }
     }
 }
+
 
 @Composable
 fun NoteCard(
@@ -51,12 +81,12 @@ fun NoteCard(
             .fillMaxWidth()
             .height(150.dp)
     ) {
-       Column(modifier = Modifier.padding(8.dp)) {
+        Column(modifier = Modifier.padding(8.dp)) {
             Text(text = note.title, style = MaterialTheme.typography.titleMedium)
             Spacer(modifier = Modifier.height(8.dp))
             Text(text = note.content)
         }
-        
+
 
     }
 }
